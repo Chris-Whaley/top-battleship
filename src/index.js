096ab3df2dc726607efb4be81b5fde2d45f358f8
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2025-06-12 19:58:41 -0400

    feat: Creating gameboards from here for DOM

--- src/index.js
+++ src/index.js
@@ -10,15 +10,10 @@ import { createBoards, createSquares } from "./dom.js";
 const playerGameboard = document.createElement("div");
 const aiGameboard = document.createElement("div");
 
-document.body.appendChild(playerGameboard);
-
 createBoards(playerGameboard, "player");
+createBoards(aiGameboard, "ai");
 
-// createBoards(playerGameboard);
-// const player = new Player();
-// const ai = new Player(false);
-
-// console.log(player.gameboard);
-// console.log(ai.gameboard);
+const player = new Player();
+const ai = new Player(false);
 
 export { Player, createSquares };
